41a285b4552455c4daf7027ff0c120e9e60b8b31
Ngoc Tu <buingoctu1998@gmail.com>
2020-07-22 14:22:14 +0700

    basic concept

---
+++ src/Controller/Controller.java
@@ -0,0 +1,11 @@
+package Controller;
+
+public class Controller {
+    private static Controller instance = new Controller();
+    public static Controller getInstance() {
+        return instance;
+    }
+    public void startApplication(){
+
+    }
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,14 @@
+package Main;
+
+import Controller.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Controller controller = Controller.getInstance();
+        try {
+            controller.startApplication();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
