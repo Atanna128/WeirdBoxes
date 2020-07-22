12eb7196fd2b2939366b84770fbf6e1e9ef74bca
HieuNT <trihieua2@gmail.com>
2020-07-22 22:41:14 +0700

    background and demo in main function

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,11 @@
 package Main;
 
 import Controller.*;
+import Model.Background;
 public class Main {
 
     public static void main(String[] args) {
+    	Background gb = new Background();
         Controller controller = Controller.getInstance();
         try {
             controller.startApplication();
--- src/Model/Background.java
+++ src/Model/Background.java
@@ -1,4 +1,46 @@
 package Model;
+import java.awt.*;  
+import java.awt.event.WindowEvent;  
+import java.awt.event.WindowListener;  
 
-public class Background {
+
+public class Background extends Frame implements WindowListener {
+	public Background(){
+		setSize(500, 450);
+		setLayout(null);
+		setVisible(true);
+		addWindowListener(this);   
+	}
+	public void windowClosing(WindowEvent e) {  
+	    dispose();  
+	}
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}  
 }
